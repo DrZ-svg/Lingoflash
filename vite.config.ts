73a5c5abd4df33156df6d60db317b32806e496b8
DrZ-svg <aliazhdari9322@gmail.com>
2025-11-29 02:51:29 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,48 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
-      },
-      manifest: {
-        name: 'LingoFlash',
-        short_name: 'LingoFlash',
-        description: 'Flashcard Language Learning App',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   base: './',
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined
-      }
-    }
-  },
-  server: {
-    host: true,
-    port: 3000
+    outDir: 'dist'
   }
 })
